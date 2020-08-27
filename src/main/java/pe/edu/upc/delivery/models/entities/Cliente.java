8956e7a9e39bf17da145e81f1d9d7a91940cbc57
Juan Flores-Moroco <ajaflorez.upc@gmail.com>
2020-08-27 11:37:18 -0500

    Added annotation @OneToMany and @ManyToOne

--- src/main/java/pe/edu/upc/delivery/models/entities/Cliente.java
+++ src/main/java/pe/edu/upc/delivery/models/entities/Cliente.java
@@ -3,15 +3,48 @@ package pe.edu.upc.delivery.models.entities;
 import java.util.Date;
 import java.util.List;
 
-public class Cliente {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {	// UpperCammel Case
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(name = "apellidos", length = 40, nullable = false)
 	private String apellidos;
+	
+	@Column(name = "nombres", length = 40, nullable = false)
 	private String nombres;
+	
+	@Column(name = "direccion", length = 50, nullable = false)
 	private String direccion;
+	
+	@ManyToOne		// 2	
+	@JoinColumn(name = "distrito_id")	// 3
 	private Distrito distrito;
-	private String numeroDocumento;
+	
+	@Column(name = "numero_documento", length = 12, nullable = false)	// Snake
+	private String numeroDocumento;	// lowerCammel Case
+	
+	@Column(name = "celular", length = 9, nullable = false)
 	private String celular;
+	
+	@Column(name = "correo", length = 40, nullable = false)
 	private String correo;
+	
+	@Column(name = "fecha_nacimiento")
+	@Temporal(TemporalType.DATE)
 	private Date fechaNacimiento;
 	
 	private List<Pedido> pedidos;
--- src/main/java/pe/edu/upc/delivery/models/entities/Distrito.java
+++ src/main/java/pe/edu/upc/delivery/models/entities/Distrito.java
@@ -2,18 +2,30 @@ package pe.edu.upc.delivery.models.entities;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "distritos")
 public class Distrito {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private Integer id;
+	
+	@Column(name = "nombre", length = 30, nullable = false)
 	private String nombre;
+	
+	@Column(name = "provincia", length = 30, nullable = false)
 	private String provincia;
 	
-	private List<Proveedor> proveedores;
+	@OneToMany(mappedBy = "distrito")		// 1(@OneToMany), 4(mappedBy)
 	private List<Cliente> clientes;
+	
+	private List<Proveedor> proveedores;
+	
 }
